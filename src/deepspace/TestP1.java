add945a9960746053ff373bb14e55329def61a2b
dkesslerm <dkesslerm03@correo.ugr.es>
2023-03-08 10:25:26 +0100

    Comienzo main

--- src/deepspace/TestP1.java
+++ src/deepspace/TestP1.java
@@ -10,7 +10,26 @@ package deepspace;
  */
 public class TestP1 {
     
-    public void main() {
+    static void main(String[] args) {
+        
+        // Clase Loot
+        
+        final int NUM_SUPPLIES = 1;
+        final int NUM_WEAPONS = 2; 
+        final int NUM_SHIELD = 1; 
+        final int NUM_HANGARS = 2;
+        final int NUM_MEDALS = 1; 
+                
+        Loot my_loot = new Loot(NUM_SUPPLIES, NUM_WEAPONS, NUM_SHIELD,
+                       NUM_HANGARS, NUM_MEDALS);
+        
+        System.out.println("El número de ");
+        int loot_num_supplies = my_loot.getNSupplies();
+        int loot_num_weapons = my_loot.getNWeapons();
+        int loot_num_shield = my_loot.getNShield();
+        int loot_num_hangars = my_loot.getNHangars();
+        int loot_num_medals = my_loot.getNMedals();
+        
         
     }
     
